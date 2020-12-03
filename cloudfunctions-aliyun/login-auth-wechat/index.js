2ae8efb25acdd76f38c0686d9ebb92fd9d7ae9de
charlesbosso <73486773+charlesbosso@users.noreply.github.com>
2020-12-03 23:23:08 +0800

    add open id

---
+++ cloudfunctions-aliyun/login-auth-wechat/index.js
@@ -0,0 +1,15 @@
+'use strict';
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	console.log('event : ', event)
+	const appid = 'wxdef61ae9500c7c5f';
+	const secret = 'c9b70367be74c45a2ae9217f3bd7cfa1';
+	const apiUrl =
+		`https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${event.code}&grant_type=authorization_code`
+	const res = await uniCloud.httpclient.request(apiUrl, {
+		dataType: 'json'
+	})
+	console.log(res)
+	//返回数据给客户端
+	return res
+};
